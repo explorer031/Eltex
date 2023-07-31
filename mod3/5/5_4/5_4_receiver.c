fcf84c06ad002822e33c325e33b5982f714eaa82
explorer031 <140680505+explorer031@users.noreply.github.com>
2023-08-01 00:42:17 +0700

    Add files via upload

---
+++ mod3/5/5_4/5_4_receiver.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define SEGMENT_SIZE 1024
+
+
+int main()
+{
+   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0)
+   {
+      perror("Unable to create socket");
+      exit(1);
+   }
+
+   struct sockaddr_in server_addr;
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = 55778;
+   server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+   if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+   {
+      perror("Unable to bind socket");
+      exit(2);
+   }
+
+   if (listen(sockfd, 1) == 0)
+      printf("Listening...\n");
+   else
+   {
+      perror("Unable to listen");
+      exit(3);
+   }
+
+   struct sockaddr_in new_addr;
+   socklen_t addr_size = sizeof(new_addr);
+   int new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
+
+   char buf[SEGMENT_SIZE];
+   FILE* fp = fopen("recv.txt", "w");
+
+   while (1)
+   {
+      int n = recv(new_sock, buf, SEGMENT_SIZE, 0);
+      if (n <= 0)
+         break;
+
+      fprintf(fp, "%s", buf);
+      bzero(buf, SEGMENT_SIZE);
+   }
+
+   printf("File received successfully.\n");
+
+   return 0;
+}
---
+++ mod3/5/5_4/5_4_sender.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define SEGMENT_SIZE 1024
+
+
+int main(int argc, char *argv[])
+{
+   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0)
+   {
+      perror("Unable to create socket");
+      exit(1);
+   }
+
+   struct sockaddr_in server_addr;
+   server_addr.sin_family = AF_INET;
+   server_addr.sin_port = 55778;
+   server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+   if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+   {
+      perror("Unable to connect to receiver");
+      exit(2);
+   }
+
+   FILE* fp = fopen(argv[2], "r");
+   if (fp == NULL)
+   {
+      perror("Unable to open file.");
+      exit(3);
+   }
+
+   char buf[SEGMENT_SIZE] = { 0 };
+
+   while (fgets(buf, SEGMENT_SIZE, fp) != NULL)
+   {
+      if (send(sockfd, buf, sizeof(buf), 0) == -1)
+      {
+         perror("Unable to send data");
+         exit(4);
+      }
+      bzero(buf, SEGMENT_SIZE);
+   }
+   
+   printf("File sent successfully.\n");
+
+   close(sockfd);
+
+   return 0;
+}
